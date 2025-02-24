2705e2bb0f4a4408678e608fac9649418c83daef
dxshaunthesheep <deshaun.tsm@gmail.com>
2025-02-24 15:48:37 +0400

    Update exercise04.py
    started working on exercise 4
    made some changes

---
+++ Exercises/exercise04.py
@@ -0,0 +1,7 @@
+## Exercise 4: Primitive Quiz - 30 Marks
+##In this exercise, you'll create a simple program that asks the user a question, evaluates their answer, and provides feedback.
+Question = (input("What is the Capital of France? : "))
+if Question : 'Paris'
+print(Question, "is the correct answer")
+else
+print("Answer is incorrect, please try again!")
