b0a554141e996331c0057eec8254df1b93218f84
dxshaunthesheep <deshaun.tsm@gmail.com>
2025-02-24 15:30:58 +0400

    Change
    added comments to exercises, completed exercise 3

--- Exercises/exercise02.py
+++ Exercises/exercise02.py
@@ -1,3 +1,5 @@
+## Exercise 2: Simple Sums - 15 Marks
+##In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
 Num1 = 8
 Num2 = 10
 Num3 = Num1+Num2
