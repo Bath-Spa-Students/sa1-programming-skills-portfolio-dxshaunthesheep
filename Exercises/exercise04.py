7e4d2e36d69767ee19868e6442203c4131c62a40
dxshaunthesheep <deshaun.tsm@gmail.com>
2025-03-22 17:42:54 +0400

    completed ex 4

--- Exercises/exercise04.py
+++ Exercises/exercise04.py
@@ -1,7 +1,24 @@
 ## Exercise 4: Primitive Quiz - 30 Marks
 ##In this exercise, you'll create a simple program that asks the user a question, evaluates their answer, and provides feedback.
-Question = (input("What is the Capital of France? : "))
-if Question : 'Paris'
-print(Question, "is the correct answer")
-else
-print("Answer is incorrect, please try again!")
+primative_quiz={"United Kingdom": "London",
+                "Germany": "Berlin",
+                "Italy": "Rome", 
+                "France": "Paris",
+                "Spain": "Madrid",
+                "Russia": "Moscow",
+                "Greece": "Athens",
+                "Portugal": "Lisbon",
+                "Netherlands": "Amsterdam",
+                "Montenegro": "Podgorica",
+}
+for country, capital in primative_quiz.items():
+    while True:
+        answer=input(f"What is the capital of {country}:?")
+        answer=answer.lower()
+
+        if answer==capital.lower():
+            print("Correct!")
+            break
+        else:
+            print("Incorrect, Please try")
+
